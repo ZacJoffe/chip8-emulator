0176971572bacff6c3689952f2fd3f66eeaa94d5
Zac Joffe <zacharyjoffe@gmail.com>
2019-12-19 22:08:11 -0500

    Added sdl2 driver code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use std::fs;
 
 use cpu::Cpu;
 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+use std::time::Duration;
+
 mod cpu;
 mod keypad;
 mod graphics;
@@ -20,6 +26,37 @@ fn main() {
     let game: Vec<u8> = fs::read(rom).expect("Couldn't find file!");
 
     cpu.load_game(game);
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("chip8", 640, 320).position_centered().build().unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                Event::MouseMotion {..} => {},
+                e => {
+                    println!("{:?}", e);
+                }
+            }
+        }
+
+        canvas.clear();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        // The rest of the game loop goes here...
+    }
 
     println!("Hello, world!");
 }
